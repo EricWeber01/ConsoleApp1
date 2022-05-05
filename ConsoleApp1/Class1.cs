6cb3d5361c9f110ab6e03729778bb7c66f0443a4
babya <babya@ERIC_WEBER>
2022-05-05 11:28:27 +0300

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Date
+    {
+        protected DateTime date { set; get; }
+    }
+    class MyDate : Date
+    {
+        public MyDate(int d, int m, int y)
+        {
+            date = new DateTime(y, m, d);
+        }
+        public bool IfLeap(DateTime date)
+        {
+            if (date.Year % 4 == 0 && (date.Year % 400 == 0 || date.Year % 100 != 0))
+            {
+                Console.WriteLine("Год высокосный");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Год не высокосный");
+                return false;
+            }
+        }
+        public bool IfToDay(DateTime date)
+        {
+            DateTime today = DateTime.Today;
+            if (date < today)
+            {
+                Console.WriteLine("Дата раньше текущей");
+                return false;
+            }
+            else if (date == today)
+            {
+                Console.WriteLine("Введена текущая дата");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Дата позже текущей");
+                return true;
+            }
+        }
+        public void PlusDay()
+        {
+            date = new DateTime(date.Year, date.Month, date.Day + 1);
+        }
+        public void Time() => Console.WriteLine(DateTime.Now.ToLongTimeString());
+        public void Date() => Console.WriteLine(DateTime.Now.ToLongDateString());
+        public void Day() => Console.WriteLine(DateTime.Now.DayOfWeek);
+    }
+    class DateInterface : MyDate
+    {
+        public DateInterface(int d, int m, int y) : base(d, m, y)
+        {
+
+        }
+        public void Show()
+        {
+            Action[] show = new Action[] { base.Time, base.Date, base.Day };
+            foreach (var item in show)
+            {
+                item();
+            }
+        }
+        public void CheckDate()
+        {
+            Predicate<DateTime>[] check = new Predicate<DateTime>[] { base.IfToDay, base.IfLeap };
+            if (check[0](base.date))
+                Console.WriteLine(" ");
+            if (check[1](base.date))
+                Console.WriteLine(" ");
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Message(string m, int c, ConsoleColor consoleColor)
+        {
+            Console.ForegroundColor = consoleColor;
+            for (int i = 0; i < c; i++)
+            {
+                Console.WriteLine(m);
+            }
+            Console.ResetColor();
+        }
+        //--------------------------------------------------------------------
+        static int Sum(int x, int y) => x + y;
+        static int Min(int x, int y) => x - y;
+        static int Mult(int x, int y) => x * y;
+        static int Div(int x, int y)
+        {
+            if (y != 0)
+                return x / y;
+            else
+                throw new DivideByZeroException();
+        }
+        //--------------------------------------------------------------------
+        static bool IsOdd(int num)
+        {
+            if (num % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+        static bool IsSimple(int num)
+        {
+            for (int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+
+                }
+            }
+            return true;
+        }
+        static bool IsFibonacci(int x)
+        {
+            double X1 = 5 * Math.Pow(x, 2) + 4;
+            double X2 = 5 * Math.Pow(x, 2) - 4;
+            long X1_sqrt = (long)Math.Sqrt(X1);
+            long X2_sqrt = (long)Math.Sqrt(X2);
+            if ((X1_sqrt * X1_sqrt == X1) || (X2_sqrt * X2_sqrt == X2))
+                return true;
+            else
+                return false;
+        }
+        static void Main(string[] args)
+        {
+            Action<string, int, ConsoleColor> messageShow;
+            messageShow = Message;
+            messageShow("Green Message", 3, ConsoleColor.Green);
+            Console.WriteLine("");
+            messageShow("Red Message", 2, ConsoleColor.Red);
+            Func<int, int, int>[] Calc = new Func<int, int, int>[4] { Sum, Min, Mult, Div };
+            foreach (var item in Calc)
+            {
+                Console.WriteLine($"10 {item.Method.Name} 5 = {item(10, 5)}");
+            }
+            Predicate<int>[] checkNum = new Predicate<int>[] { IsOdd, IsSimple, IsFibonacci };
+            Console.WriteLine($"(IsOdd)checkNum[0](2) - {checkNum[0](3)}");
+            Console.WriteLine($"(IsSimple)checkNum[1](11) - {checkNum[1](11)}");
+            Console.WriteLine($"(IsFibonacci)checkNum[2](8) - {checkNum[2](8)}");
+            DateInterface d = new DateInterface(6, 4, 2020);
+            d.Show();
+            d.CheckDate();
+        }
+    }
+}
